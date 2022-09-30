6ebfa286ff57a460fb479decd17514bea6a60080
Olusola Oseni <kenolusola@gmail.com>
2022-09-30 12:04:15 +0100

    Implement signin component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/core/Home";
 import { Users } from "./components/user/Users";
 import { Signup } from "./components/user/Signup";
+import { Signin } from "./components/auth/Signin";
 
 export const MainRouter = () => {
   return (
@@ -11,6 +12,7 @@ export const MainRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     </div>
   );
